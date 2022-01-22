f948873146fd563179abb161e24ad536d35c60ba
Mukesh <mukeshbhati24@gmail.com>
2022-01-23 00:14:02 +0530

    added router dom

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import path from "path";
 
 import React from "react";
 import ReactDOMServer from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
 
 import App from "../src/App";
 
@@ -12,16 +13,19 @@ const PORT = 8000;
 const app = express();
 
 app.use("^/$", (req, res, next) => {
+  const context = {};
+  const app = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
   fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
     if (err) {
       console.log(err);
       return res.status(500).send("Some error happened");
     }
     return res.send(
-      data.replace(
-        '<div id="root"></div>',
-        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
-      )
+      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
     );
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
   return (
     <>
-      <div className="App">
-        <p>{count}</p>
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about" style={{ paddingLeft: "20px" }}>
+          About
+        </Link>
       </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </>
   );
 }
